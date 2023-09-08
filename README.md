f6775bd6406b277c3624910511ad5ee99fb3929a
idanv87 <idanv@campus.technion.ac.il>
2023-09-08 23:10:54 +0300

    first

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Example Package
+To rebuild go the the location of .toml file:
+python -m build 
+
+To install the package:
+pip install /Users/idanversano/Documents/my_package/dist/idan_package-0.0.1.tar.gz
+
+to uninstall:
+pip uninstall idan_package
+
--- src/package/utils.py
+++ src/package/utils.py
@@ -1,5 +1,41 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def add(x):
-    print(x)
+def plot_axis(ax,x,y, **kwargs):
+
+    d=kwargs
+    try:
+        ax.plot(x,y, color=d['color'],  label=d['label'], linestyle=d['line_style'])
+        ax.legend()
+    except:    
+         ax.plot(x,y, color=d['color'])
+    try:     
+        ax.set_title(d['title'])
+    except:
+         pass    
+    try:
+        ax.set_xlabel(d['xlabel'])
+        ax.set_ylabel(d['ylabel']) 
+    except:
+         pass    
+    # f'err={y[-1]:.2e}'
+    try:
+        ax.text(d['x_text'], d['y_text'], d['text'], c=d['color'])
+    except:
+        pass  
+
+def plot_arrow(ax, **kwargs):
+        d=kwargs
+        ax.annotate('',
+                xy = d['end'],
+                xytext =d['start'],
+                arrowprops = dict(facecolor = 'black', width = 0.2, headwidth = 8),
+                horizontalalignment = 'center') 
+
+
+def save_eps(path):
+        plt.savefig(path, format='eps',bbox_inches='tight')
+        plt.show(block=False)
+
+
+ 
